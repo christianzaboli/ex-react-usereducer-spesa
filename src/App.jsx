cfcb0f9c5239c7b927636b853f0ace8a20677718
christianzaboli <zabolichristian@gmail.com>
2025-12-24 03:42:59 +0100

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,39 @@ const products = [
   { name: "Pasta", price: 0.7 },
 ];
 function App() {
+  const [addedProducts, setAddedProducts] = useState([]);
+  function addToCart(i) {
+    const cartItem = { ...products[i], quantity: 1 };
+    if (
+      addedProducts.length > 0 &&
+      addedProducts.some((e) => e.name === cartItem.name)
+    ) {
+      return;
+    } else {
+      setAddedProducts([...addedProducts, cartItem]);
+      console.log(addedProducts.includes(cartItem));
+    }
+  }
   return (
     <>
       <main>
+        <h3>Lista prodotti</h3>
         <ul>
-          {products.map((p) => {
+          {products.map((p, i) => {
             return (
-              <li>
+              <li key={i}>
                 Nome: {p.name} Prezzo: {p.price}$
+                <button onClick={() => addToCart(i)}>+</button>
+              </li>
+            );
+          })}
+        </ul>
+        <h3>Carrello</h3>
+        <ul>
+          {addedProducts?.map((p, i) => {
+            return (
+              <li key={i}>
+                Nome: {p.name}, Prezzo: {p.price}, Quantitá: {p.quantity}
               </li>
             );
           })}
