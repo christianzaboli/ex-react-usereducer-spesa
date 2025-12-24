904cf64fe851876040801300dd01498d7bf738ab
christianzaboli <zabolichristian@gmail.com>
2025-12-24 02:45:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useReducer } from "react";
+
+const products = [
+  { name: "Mela", price: 0.5 },
+  { name: "Pane", price: 1.2 },
+  { name: "Latte", price: 1.0 },
+  { name: "Pasta", price: 0.7 },
+];
+function App() {
+  return <></>;
+}
+
+export default App;
