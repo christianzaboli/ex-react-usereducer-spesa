63df4b49945a2017e178ed3b6b711e8c95adc14d
osaze <osazezaboli@gmail.com>
2025-12-31 00:32:54 +0100

    milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,31 @@ const products = [
   { name: "Latte", price: 1.0 },
   { name: "Pasta", price: 0.7 },
 ];
+
 function App() {
   const [addedProducts, setAddedProducts] = useState([]);
+
   function addToCart(i) {
-    const cartItem = { ...products[i], quantity: 1 };
-    if (
-      addedProducts.length > 0 &&
-      addedProducts.some((e) => e.name === cartItem.name)
-    ) {
-      return;
-    } else {
-      setAddedProducts([...addedProducts, cartItem]);
-      console.log(addedProducts.includes(cartItem));
-    }
+    const product = products[i];
+
+    setAddedProducts((prev) =>
+      prev.some((item) => item.name === product.name)
+        ? prev.map((item) =>
+            item.name === product.name
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          )
+        : [...prev, { ...product, quantity: 1 }]
+    );
   }
+
+  function removeFromCart(name) {
+    setAddedProducts((prev) => prev.filter((item) => item.name !== name));
+  }
+
+  let sumPay = addedProducts.reduce((tot, curr) => {
+    return tot + curr.price * curr.quantity;
+  }, 0);
   return (
     <>
       <main>
@@ -40,10 +51,12 @@ function App() {
             return (
               <li key={i}>
                 Nome: {p.name}, Prezzo: {p.price}, Quantitá: {p.quantity}
+                <button onClick={() => removeFromCart(p.name)}>X</button>
               </li>
             );
           })}
         </ul>
+        <h4>Totale: {sumPay.toFixed(2)}</h4>
       </main>
     </>
   );
